2e4d3282fff917b2487b9a58f24b49e9f8224617
Kakoen <kakoen@gmail.com>
2021-03-05 15:43:06 +0100

    Slightly optimized writing ZDOs without creating a new HashMap for each of them.

--- src/main/java/net/kakoen/valheim/save/archive/save/Zdo.java
+++ src/main/java/net/kakoen/valheim/save/archive/save/Zdo.java
@@ -194,32 +194,22 @@ public class Zdo {
 	}
 	
 	private <T> void writeProperties(ZPackage writer, Map<Integer, T> valuesByHash, Map<String, T> valuesByName, Consumer<T> writeFunction) {
-		Map<Integer, T> map = new LinkedHashMap<>();
+		int size = ((valuesByHash != null) ? valuesByHash.size() : 0)
+				 + ((valuesByName != null) ? valuesByName.size() : 0);
+		
+		writer.writeChar(size);
 		if(valuesByHash != null) {
-			map.putAll(valuesByHash);
+			valuesByHash.forEach((k, v) -> {
+				writer.writeInt32(k);
+				writeFunction.accept(v);
+			});
 		}
 		if(valuesByName != null) {
 			valuesByName.forEach((k, v) -> {
-				map.put(StableHashCode.getStableHashCode(k), v);
+				writer.writeInt32(StableHashCode.getStableHashCode(k));
+				writeFunction.accept(v);
 			});
 		}
-		writer.writeChar(map.size());
-		map.forEach((k, v) -> {
-			writer.writeInt32(k);
-			writeFunction.accept(v);
-		});
-	}
-	
-	private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
-	public static String bytesToHex(byte[] bytes) {
-		byte[] hexChars = new byte[bytes.length * 3];
-		for (int j = 0; j < bytes.length; j++) {
-			int v = bytes[j] & 0xFF;
-			hexChars[j * 3] = HEX_ARRAY[v >>> 4];
-			hexChars[j * 3 + 1] = HEX_ARRAY[v & 0x0F];
-			hexChars[j * 3 + 2] = ' ';
-		}
-		return new String(hexChars, StandardCharsets.UTF_8);
 	}
 }
 
