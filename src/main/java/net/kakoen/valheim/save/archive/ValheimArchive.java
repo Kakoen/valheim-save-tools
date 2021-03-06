c018de0f657cb4128fb9f7cdc0554dee16d34fb5
Kakoen <kakoen@gmail.com>
2021-03-06 14:00:38 +0100

    Fixed type property appearing twice in output json.

--- src/main/java/net/kakoen/valheim/save/archive/ValheimArchive.java
+++ src/main/java/net/kakoen/valheim/save/archive/ValheimArchive.java
@@ -3,12 +3,12 @@ package net.kakoen.valheim.save.archive;
 import java.io.File;
 import java.io.IOException;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
-@JsonTypeInfo(use = Id.NAME, include = As.PROPERTY, property = "type", visible = true)
+@JsonTypeInfo(use = Id.NAME, property = "type")
 @JsonSubTypes({
 		@JsonSubTypes.Type(value = ValheimCharacter.class, name = "FCH"),
 		@JsonSubTypes.Type(value = ValheimSaveArchive.class, name = "DB"),
@@ -18,10 +18,7 @@ public interface ValheimArchive {
 	
 	void save(File outputFile) throws IOException;
 	
-	default void setType(ValheimArchiveType type) {
-		//Do nothing, as type is read-only. But for deserialization, this is required for some reason
-	}
-	
+	@JsonIgnore
 	ValheimArchiveType getType();
 	
 }
