f64f033e17b61c84a13d16c7fd8f8a48cf722531
Kakoen <kakoen@gmail.com>
2021-03-07 10:36:57 +0100

    Maintenance: Added doc to ValheimArchiveProcessor

--- src/main/java/net/kakoen/valheim/save/processor/ValheimArchiveProcessor.java
+++ src/main/java/net/kakoen/valheim/save/processor/ValheimArchiveProcessor.java
@@ -6,8 +6,21 @@ import net.kakoen.valheim.save.archive.ValheimArchiveType;
 
 public interface ValheimArchiveProcessor {
 	
-	boolean isEnabled(SaveToolsCLIOptions options);
-	void process(ValheimArchive archive, SaveToolsCLIOptions options);
+	/**
+	 * Returns on which archive type the processor can be executed.
+	 */
 	ValheimArchiveType getType();
+	
+	/**
+	 * Returns whether the processor can be applied based on the given options. There's no need to
+	 * do an archive type check here.
+	 */
+	boolean isEnabled(SaveToolsCLIOptions options);
+	
+	/**
+	 * Processes the given archive. At this point you can be certain that the type of the archive and the
+	 * enabled state have been checked.
+	 */
+	void process(ValheimArchive archive, SaveToolsCLIOptions options);
 	
 }
