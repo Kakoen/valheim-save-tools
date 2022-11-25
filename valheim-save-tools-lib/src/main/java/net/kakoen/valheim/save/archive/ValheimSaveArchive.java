d3cabeb95ebeab6df8205b7a2ff03a4f255e647e
Kakoen <kakoen@gmail.com>
2022-11-25 18:54:44 +0100

    Bump max tested world version to 28

--- valheim-save-tools-lib/src/main/java/net/kakoen/valheim/save/archive/ValheimSaveArchive.java
+++ valheim-save-tools-lib/src/main/java/net/kakoen/valheim/save/archive/ValheimSaveArchive.java
@@ -26,7 +26,7 @@ import net.kakoen.valheim.save.parser.ZPackage;
 @Slf4j
 public class ValheimSaveArchive implements ValheimArchive {
 
-	public static final int MAX_SUPPORTED_WORLD_VERSION = 27;
+	public static final int MAX_SUPPORTED_WORLD_VERSION = 28;
 	
 	private Meta meta;
 	private long modified;
