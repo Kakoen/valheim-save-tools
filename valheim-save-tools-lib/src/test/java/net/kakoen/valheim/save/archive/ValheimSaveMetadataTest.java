059bb91ed71c5c41bc41d2dc4edd81f013387d7d
kakoen <kakoen@gmail.com>
2023-04-21 19:00:52 +0200

    #70 Bumped supported world version to 29

--- valheim-save-tools-lib/src/test/java/net/kakoen/valheim/save/archive/ValheimSaveMetadataTest.java
+++ valheim-save-tools-lib/src/test/java/net/kakoen/valheim/save/archive/ValheimSaveMetadataTest.java
@@ -16,18 +16,18 @@ public class ValheimSaveMetadataTest {
 	
 	@Test
 	public void valheimSaveMetadata_shouldCorrectlyLoadMetadata() throws IOException, ValheimArchiveUnsupportedVersionException {
-		File inFile = new File("src/test/resources/Test20210421.fwl");
+		File inFile = new File("src/test/resources/Test.fwl");
 		ValheimSaveMetadata metadata = new ValheimSaveMetadata(inFile, new ValheimArchiveReaderHints());
-		Assertions.assertEquals(27, metadata.getWorldVersion());
-		Assertions.assertEquals(1, metadata.getWorldGenVersion());
-		Assertions.assertEquals("Test20210421", metadata.getName());
-		Assertions.assertEquals("xbDKqu4XSu", metadata.getSeedName());
-		Assertions.assertEquals(-931666925, metadata.getSeed());
+		Assertions.assertEquals(29, metadata.getWorldVersion());
+		Assertions.assertEquals(2, metadata.getWorldGenVersion());
+		Assertions.assertEquals("Test", metadata.getName());
+		Assertions.assertEquals("fGSaPHCYaR", metadata.getSeedName());
+		Assertions.assertEquals(555290274, metadata.getSeed());
 	}
 	
 	@Test
 	public void valheimSaveMetadata_shouldCorrectlySaveMetadata() throws IOException, ValheimArchiveUnsupportedVersionException {
-		File inFile = new File("src/test/resources/Test20210421.fwl");
+		File inFile = new File("src/test/resources/Test.fwl");
 		ValheimSaveMetadata metadata = new ValheimSaveMetadata(inFile, new ValheimArchiveReaderHints());
 		File outFile = File.createTempFile("out", ".fwl");
 		metadata.save(outFile);
